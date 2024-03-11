380d520f341981a4aa7db8e7438e2b22eb2bfb9a
Vitaly <xaleralizard@gmail.com>
2024-03-12 00:15:10 +0300

    Change structure solution.

--- Logic/Extensions/Extensions.cs
+++ Logic/Extensions/Extensions.cs
@@ -11,11 +11,7 @@ namespace Logic.Extensions
         public static Period GetPeriod(this string date)
         {
             const string format = @"(eq|ne|gt|lt|ge|le|sa|eb|ap)?([0-9]([0-9]([0-9][1-9]|[1-9]0)|[1-9]00)|[1-9]000)(-(0[1-9]|1[0-2])(-(0[1-9]|[1-2][0-9]|3[0-1])(T([01][0-9]|2[0-3]):[0-5][0-9]:([0-5][0-9]|60)(\.[0-9]{1,9})?)?)?(Z|(\+|-)((0[0-9]|1[0-3]):[0-5][0-9]|14:00)?)?)?";
-
-            Console.WriteLine("------------------------");
-            Console.WriteLine(date);
-            Console.WriteLine("------------------------");
-
+            
             Regex regex = new Regex(format);
             if (regex.IsMatch(date) == false)
                 throw new ArgumentException("Parametr DateTime not valid.");
