7e7508a571f880e3ee7e1ab2b8150e34389188e2
Vitaly <xaleralizard@gmail.com>
2024-03-12 02:59:41 +0300

    Make code cleanup.

--- src/Core/ApplicationContext.cs
+++ src/Core/ApplicationContext.cs
@@ -22,7 +22,7 @@ namespace Core
         #region Overriding
         protected override void OnConfiguring(DbContextOptionsBuilder builder)
         {
-            builder.UseNpgsql("Host=localhost;Port=5432;Database=db;Username=postgres;Password=FDgsd32Fkfsj2kfkv11");
+            builder.UseNpgsql("Host=postgres;Port=5432;Database=db;Username=postgres;Password=FDgsd32Fkfsj2kfkv11");
 
             base.OnConfiguring(builder);
         }
